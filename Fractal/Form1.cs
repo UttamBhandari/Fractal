90963141cabcd3d451689e6c0a42616b8edd34ac
UttamBhandari <uttambhandari75@gmail.com>
2018-03-19 22:06:28 +0545

    mandelbrot added with hsb new class

--- Fractal/Form1.cs
+++ Fractal/Form1.cs
@@ -13,10 +13,11 @@ namespace Fractal
 {
     public partial class Form1 : Form
     {
-        private int MAX = 256;
-        private double SX = -2.025;
-        private double SY = -1.125;
-        private double EX = 0.6;
+        private int MAX = 256; // max iterations
+        private double SX = -2.025;  // start value real
+        private double SY = -1.125; // start value imaginary
+        private double EX = 0.6;  // end value real
+        private double EY = 1.125;  // end value imaginary
         private static int x1 ,y1,xs,ys,xe,ye;
         private static double xstart, ystart, xende, yende, xzoom, yzoom;
         private static bool action, rectangle, finished;
@@ -24,12 +25,57 @@ namespace Fractal
 
         private Image picture;
         private Graphics g1;
-        private Cursor c1;
-        private Cursor c2;
+        private Cursor c1,c2;
+        private HSB HSBcol;
         public Form1()
         {
             InitializeComponent();
+            init();
+            start();
         }
+        public void init()
+        {
+
+        }
+        public void start()
+        {
+            action = false;
+            rectangle = false;
+            initvalues();
+            xzoom = (xende - xstart) / (double)x1;
+            yzoom = (yende - ystart) / (double)y1;
+            mandelbrot();
+        }
+        private void initvalues() //reset start value
+        {
+            xstart = SX;
+            ystart = SY;
+            xende = EX;
+            yende = EY;
+            if ((float)((xende - xstart) / (yende - ystart)) != xy)
+                xstart = xende - (yende - ystart) * (double)xy;
+        }
+
+        private void mandelbrot() //calculate all points
+        {
+            int x, y;
+            float h, b, alt = 0.0f;
+            action = false;
+            //pictureBox1.Cursor = c1; ( java cursor)
+            pictureBox1.Cursor = c2;
+
+            //showStatus("Mandelbrot-Set will be produced - please wait...");
+            for (x = 0; x < x1; x += 2)
+                for (y = 0; y < y1; y++)
+                {
+                    h = pointcolour(xstart + xzoom * (double)x, ystart + yzoom * (double)y); // color value
+                    if (h != alt)
+                    {
+                        b = 1.0f - h * h; // brightnes
+                                          //djm added
+                        HSBcol.fromHSB(h, 0.8f, b);
+                        Color col = new Color(0, HSBcol.rChan, HSBcol.gChan, HSBcol.bChan);
+                    }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
---
+++ Fractal/HSB.cs
@@ -0,0 +1,74 @@
+﻿namespace Fractal
+{
+    internal class HSB
+    {
+        public float rChan, gChan, bChan;
+        public HSB()
+        {
+            rChan = gChan = bChan = 0;
+        }
+        public void fromHSB(float h, float s, float b)
+        {
+            float red = b;
+            float green = b;
+            float blue = b;
+            if (s != 0)
+            {
+                float max = b;
+                float dif = b * s / 255;
+                float min = b - dif;
+                float h2 = h * 360 / 255;
+                if (h2 < 60)
+                {
+                    red = max;
+                    green = h2 * dif / 60 + min;
+                    blue = min;
+                }
+                else if(h2 < 120){
+                    red = (h2 - 120) * dif / 60  * -1+ min;
+                    green = max;
+                    blue = min;
+                }
+                else if (h2 < 180)
+                {
+                    red = min;
+                    green = max;
+                    blue = (((h2 - 120)
+                                * (dif / 60))
+                                + min);
+                }
+                else if ((h2 < 240))
+                {
+                    red = min;
+                    green = ((((h2 - 240)
+                                * (dif / 60))
+                                * -1)
+                                + min);
+                    blue = max;
+                }
+                else if ((h2 < 300))
+                {
+                    red = (((h2 - 240)
+                                * (dif / 60))
+                                + min);
+                    green = min;
+                    blue = max;
+                }
+                else if ((h2 <= 360))
+                {
+                    red = max;
+                    green = min;
+                    blue = ((((h2 - 360)
+                                * (dif / 60))
+                                * -1)
+                                + min);
+                }
+                else {
+                    red = 0;
+                    green = 0;
+                    blue = 0;
+                }
+            }
+            }
+    }
+}
